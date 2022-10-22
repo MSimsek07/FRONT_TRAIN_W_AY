30507dbc41b81c7a72d0ed6541dcd0f658d654b5
Ahmet yıldız <7207.yildizqgmail.com>
2022-10-23 01:37:06 +0300

    Ahmet

--- train-app/src/App.js
+++ train-app/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+ import * as Components from './Components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ function App() {
+     const [signIn, toggle] = React.useState(true);
+      return(
+          <Components.Container>
+              <Components.SignUpContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Kayıt Ol</Components.Title>
+                      <Components.Input type='text' placeholder='Ad' />
+                      <Components.Input type='text' placeholder='Soyad' />
+                      <Components.Input type='text' placeholder='Kullanıcı Adı' />
+                      <Components.Input type='email' placeholder='E-posta' />
+                      <Components.Input type='password' placeholder='Şifre' />
+                      <Components.Input type='password' placeholder='Şifre Tekrar' />
+                      <Components.Input type='date' placeholder='Dogum Tarihi' >
+                        
+                      </Components.Input>
+                      <Components.Input type='text' placeholder='cinsiyet' />
+                      <Components.Button>Üye Ol</Components.Button>
+                  </Components.Form>
+              </Components.SignUpContainer>
 
-export default App;
+              <Components.SignInContainer signinIn={signIn}>
+                   <Components.Form>
+                       <Components.Title>Giriş Yap</Components.Title>
+                       <Components.Input type='email' placeholder='E-posta' />
+                       <Components.Input type='password' placeholder='Şifre' />
+                       <Components.Anchor href='#'>Şifrenizi mi unuttunuz?</Components.Anchor>
+                       <Components.Button>Giriş Yap</Components.Button>
+                   </Components.Form>
+              </Components.SignInContainer>
+
+              <Components.OverlayContainer signinIn={signIn}>
+                  <Components.Overlay signinIn={signIn}>
+
+                  <Components.LeftOverlayPanel signinIn={signIn}>
+                      <Components.Title>1001FİLM {<br/>}Hoşgeldiniz </Components.Title>
+                      <Components.Paragraph>
+                          1001FİLM insanların filimler hakkında konuştuğu sosyal bir platformdur.
+                      </Components.Paragraph>
+                      <Components.GhostButton onClick={() => toggle(true)}>
+                          Giriş Yap
+                      </Components.GhostButton>
+                      </Components.LeftOverlayPanel>
+
+                      <Components.RightOverlayPanel signinIn={signIn}>
+                        <Components.Title>Selamlar, Arkadaşlar</Components.Title>
+                        <Components.Paragraph>
+                        Kişisel bilgilerinizi girin ve bizimle yolculuğa başlayın
+                        </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Üye Ol
+                            </Components.GhostButton> 
+                      </Components.RightOverlayPanel>
+  
+                  </Components.Overlay>
+              </Components.OverlayContainer>
+
+          </Components.Container>
+      )
+ }
+
+ export default App;
